2db6dbdab0e70a7caf4b91ec7235824a21473e6f
Lucas de mello <lucas_mello6@hotmail.com>
2022-05-06 12:51:02 -0300

    Classroom service

--- classroom/src/http/http.module.ts
+++ classroom/src/http/http.module.ts
@@ -3,8 +3,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import path from 'path';
+import { CoursesService } from 'src/services/courses.service';
+import { EnrollmentsService } from 'src/services/enrollments.service';
+import { StudentsService } from 'src/services/students.service';
 import { DatabaseModule } from '../database/database.module';
-import { TestResolver } from './test.resolver';
+import { CoursesResolver } from './graphql/resolvers/courses.resolver';
+import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
+import { StudentsResolver } from './graphql/resolvers/students.resolver';
 
 @Module({
   imports: [
@@ -15,6 +20,13 @@ import { TestResolver } from './test.resolver';
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
-  providers: [TestResolver],
+  providers: [
+    StudentsResolver,
+    EnrollmentsResolver,
+    CoursesResolver,
+    CoursesService,
+    EnrollmentsService,
+    StudentsService,
+  ],
 })
 export class HttpModule {}
---
+++ classroom/src/services/enrollments.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+
+interface GetByCourseStudentIdParams {
+  courseId: string;
+  studentId: string;
+}
+
+@Injectable()
+export class EnrollmentsService {
+  constructor(private prisma: PrismaService) {}
+
+  listAllEnrollments() {
+    return this.prisma.enrollment.findMany({
+      where: {
+        canceledAt: null,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
+
+  getByCourseAndStudentId({ courseId, studentId }: GetByCourseStudentIdParams) {
+    return this.prisma.enrollment.findFirst({
+      where: {
+        courseId,
+        studentId,
+        canceledAt: null,
+      },
+    });
+  }
+
+  listEnrollmentsByStudentId(studentId: string) {
+    return this.prisma.enrollment.findMany({
+      where: {
+        studentId,
+        canceledAt: null,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
+}
